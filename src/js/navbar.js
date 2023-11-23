790bd3dccd298f5548297f69e4f5f28c5396ba3c
[KIMCHANGHWAN] <98susckdghks@naver.com>
2023-11-23 16:58:55 +0900

    로그인 여부에 따라 로그인/ 로그아웃 버튼 토글

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -26,3 +26,26 @@ function logout() {
   alert("로그아웃 되었습니다.");
   location.reload();
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  function getCookie(name) {
+    const cookieString = document.cookie;
+    const cookies = cookieString.split(";").map((cookie) => cookie.trim());
+
+    for (const cookie of cookies) {
+      const [key, value] = cookie.split("=");
+      if (key === name) {
+        return value;
+      }
+    }
+
+    return null;
+  }
+
+  const access = getCookie("access");
+
+  if (access) {
+    $loginbutton.style.display = "none";
+    $logoutbutton.style.display = "inline-block";
+  }
+});
