a81af01dee87d8da78eeab7f2d0694dc2ec5587e
[KIMCHANGHWAN] <98susckdghks@naver.com>
2023-11-28 17:34:38 +0900

    도메인 연결, https 테스트

--- src/js/urls.js
+++ src/js/urls.js
@@ -1,5 +1,5 @@
 //url 정리
-const url = "http://43.202.169.34";
+const url = "https://blood-donation-day.com";
 const profileurl = url + "/accounts/profile/";
 const loginurl = url + "/accounts/login/";
 const signupurl = url + "/accounts/signup/";
